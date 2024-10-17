57f8e0bb7d41c6f68b611d12e12d903d7ca3ba4c
Vinicius Santiago <vinicius.santiago@sap.com>
2024-10-17 12:22:30 -0300

    feat: region and tags required in public subnet

--- terraform-aws-eks/public.tf
+++ terraform-aws-eks/public.tf
@@ -1,23 +1,25 @@
 resource "aws_subnet" "vini_eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.vini_eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "vini-eks-subnet-public-1a"
-    Project = "vini-eks"
+    Name                     = "vini-eks-subnet-public-1a",
+    Project                  = "vini-eks",
+    "kubernetes.io/role/elb" = 1
   }
 }
 
 resource "aws_subnet" "vini_eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.vini_eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name    = "vini-eks-subnet-public-1b"
-    Project = "vini-eks"
+    Name                     = "vini-eks-subnet-public-1b",
+    Project                  = "vini-eks",
+    "kubernetes.io/role/elb" = 1
   }
 }
