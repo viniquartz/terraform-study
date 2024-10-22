2019dabd9cf6ef37bc8c13cca3574aafd86ce585
Vinicius Santiago <vinicius.santiago@sap.com>
2024-10-22 09:16:01 -0300

    feat: eks oidc

--- terraform-aws-eks/modules/cluster/oidc.tf
+++ terraform-aws-eks/modules/cluster/oidc.tf
@@ -3,7 +3,9 @@ data "tls_certificate" "vini_eks_cluster_oidc_tls_certificate" {
 }
 
 resource "aws_iam_openid_connect_provider" "vini_eks_cluster_oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
   thumbprint_list = [data.tls_certificate.vini_eks_cluster_oidc_tls_certificate.certificates[0].sha1_fingerprint]
   url             = data.tls_certificate.vini_eks_cluster_oidc_tls_certificate.url
 }
